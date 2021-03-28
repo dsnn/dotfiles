e910ffcbd7a6187c9d8b6e1c560568fff58854d3
dsn <dsn@dsnn.io>
2021-03-28 13:11:39 +0200

    Update readme and plugins

--- README.md
+++ README.md
@@ -12,6 +12,8 @@ tmux: ctrl+a ctrl+I to install tmux packages
 
 ## LSP for nvim 
 
+`npm install --prefix ~/.local/share/nvim/lspinstall/ <pkg1, pkg2>`
+
 ```vim
 npm i -g bash-language-server \
 vscode-css-languageserver-bin \
@@ -44,22 +46,46 @@ markdownlint --save-dev
 
 ## TODO
 
-- review / fix install script for *
-- autoinstall nvim deps (e.g. lsp)
-- update for nightly builds (.local/bin)
-- add floaterm w/ lazy*
-- fix Bdelete update tab
-- telescope-arecibo 
-- ueberzug 
-- fd
-- ripgrep
-- jq
-- lazygit
-- lazydocker
-- lazynpm
-- ncdu
-- pynvim
-- neovim-remote
-- pandoc
-- latex support
-- omnisharp
+- [ ] review / fix install script for *
+	- write bash script for w/e. (make it clean and pretty w/ separate functions for w/e)
+	- simple install solution: call script fns from makefile?
+- [ ] autoinstall/updates for
+    	- automatic daily update (might be "dangerous" or annoying if things break ?)
+	    	- on first run for the day ?
+		- cron ?
+	- lsp-servers 
+	- app deps (make install now)
+	- nightly build apps (.local/bin)
+	- repos / submodules
+- [ ] add floaterm w/ lazy*
+- [ ] table for nvim plugins 
+- [ ] cheat.sh (https://github.com/nvim-telescope/telescope-cheat.nvim)
+	- regex
+	- work stuff (e.g. db connection strings. submodule)
+- [ ] nvim surround plugin
+- [ ] markdown lint / lsp / snippets / preview
+- [ ] better quickfix win 
+    - telescope?
+    - lsp, typechecks, linters
+- [ ] telescope-arecibo 
+- [ ] vim which key (bind to e.g. space + char ?) 
+- [ ] fix Bdelete update tab
+- [ ] vimwiki 
+	- readme
+	- todos
+	- notes for home / work (submodule) ?
+	- eleventy (submodule) ?
+- [ ] failsafe for aliases (e.g. check if lazygit exists)
+- [ ] shellcheck 
+- [ ] ueberzug 
+- [ ] fd (aka fd-find)
+- [ ] ripgrep - error w/ batcat, tmp fix: `sudo apt install -o Dpkg::Options::="--force-overwrite" bat ripgrep`
+- [ ] lazygit
+- [ ] lazydocker
+- [ ] lazynpm
+- [ ] pynvim
+- [ ] neovim-remote (open files from :term witout nesting)
+- [ ] pandoc
+- [ ] latex support
+- [ ] omnisharp
+- [ ] unzip (required by lua lsp server)
--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -10,46 +10,58 @@ end
 return require('packer').startup(function(use)
     -- Packer can manage itself as an optional plugin
     use 'wbthomason/packer.nvim'
-    use 'TimUntersberger/neogit'
-    use 'airblade/vim-rooter'
-    use 'andymass/vim-matchup'
-    -- use 'bfredl/nvim-miniyank'
-    -- use 'christianchiarulli/emmet-vim'
-    use 'christianchiarulli/nvcode-color-schemes.vim' 
-    use 'dsznajder/vscode-es7-javascript-react-snippets'
-    -- use 'gennaro-tedesco/nvim-peekup'
-    use 'glepnir/galaxyline.nvim'
-    use 'glepnir/lspsaga.nvim'
-    use 'hrsh7th/nvim-compe'
-    use 'hrsh7th/vim-vsnip'
+
+    -- general
     use 'junegunn/goyo.vim'
-    use 'kevinhwang91/nvim-bqf'
-    use 'kevinhwang91/rnvimr'
-    use 'kosayoda/nvim-lightbulb'
-    use 'kyazdani42/nvim-tree.lua'
-    use 'kyazdani42/nvim-web-devicons'
-    use 'liuchengxu/vim-which-key'
-    use 'liuchengxu/vista.vim'
     use 'mhinz/vim-startify'
+    use 'terrortylor/nvim-comment'
+    use 'voldikss/vim-floaterm'
+    -- use 'bfredl/nvim-miniyank'
+
+    -- buffer, register, tabs & statusline
     use 'moll/vim-bbye'
+    use 'glepnir/galaxyline.nvim'
+    use 'romgrk/barbar.nvim'
+    use 'gennaro-tedesco/nvim-peekup' 				-- peek on registers
+
+    -- navigation
+    use 'airblade/vim-rooter'  					-- cwd to root on open 
+    use 'kevinhwang91/rnvimr'
+    use 'kyazdani42/nvim-tree.lua'
+    use 'liuchengxu/vim-which-key'
+    use 'unblevable/quick-scope'
+    use 'andymass/vim-matchup'
+
+    -- icons, colors & themes
+    use 'kyazdani42/nvim-web-devicons'
+    use 'ryanoasis/vim-devicons'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'tjdevries/colorbuddy.vim'
+    use 'christianchiarulli/nvcode-color-schemes.vim' 
+    use 'sainnhe/sonokai'
+
+    -- lsp & code 
+    use 'glepnir/lspsaga.nvim'
     use 'neovim/nvim-lspconfig'
     use 'kabouzeid/nvim-lspinstall'
-    use 'norcalli/nvim-colorizer.lua'
+    use 'onsails/lspkind-nvim'
+    use 'kosayoda/nvim-lightbulb' 
+    use 'sheerun/vim-polyglot'					-- language pack
+    use 'liuchengxu/vista.vim'  				-- lsp symbols & tags
+    use 'kevinhwang91/nvim-bqf' 				-- better quick fix window
+
+    -- telescope
+    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
     use 'nvim-lua/plenary.nvim'
     use 'nvim-lua/popup.nvim'
     use 'nvim-telescope/telescope-media-files.nvim'
     use 'nvim-telescope/telescope.nvim'
-    use 'onsails/lspkind-nvim'
-    use 'romgrk/barbar.nvim'
-    use 'ryanoasis/vim-devicons'
-    use 'sainnhe/sonokai'
-    use 'sheerun/vim-polyglot'	
-    use 'terrortylor/nvim-comment'
-    use 'tjdevries/colorbuddy.vim'
-    use 'tpope/vim-fugitive'
-    use 'unblevable/quick-scope'
-    use 'voldikss/vim-floaterm'
-    use 'windwp/nvim-autopairs'
+
+    -- autocomplete
+    use 'hrsh7th/nvim-compe'
+    use 'hrsh7th/vim-vsnip'
+    use 'dsznajder/vscode-es7-javascript-react-snippets'
     use 'xabikos/vscode-javascript'
-    use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
+    use 'windwp/nvim-autopairs'
+    -- use 'christianchiarulli/emmet-vim'
 end)

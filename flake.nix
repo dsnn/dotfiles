fd25da569e0f4bd22ea0c607502030fa63c222df
dsn <dsn@dsnn.io>
2022-01-30 17:19:41 +0100

    Fix formatting for nix

--- flake.nix
+++ flake.nix
@@ -6,42 +6,16 @@
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
-    # let mkHomeConfiguration = args: home-manager.lib.homeManagerConfiguration {
-    #     system = args.system or "x86_64-linux";
-    #     # configuration = import ./home.nix;
-    #     homeDirectory = "/home/dsn";
-    #     username = "dsn";
-    #     stateVersion = "22.05";
-    #   };
-    {
-
-      homeConfigurations = {
-        dsn = inputs.home-manager.lib.homeManagerConfiguration {
-          system = "x86_64-linux";
-          homeDirectory = "/home/dsn";
-          username = "dsn";
-          stateVersion = "22.05";
-          configuration = { config, pkgs, lib, ... }: {
-            imports = [ ./home.nix ];
-          };
-        };
+  outputs = { self, pkgs, home-manager, ... }: {
+    homeConfigurations = {
+      dsn = home-manager.lib.homeManagerConfiguration {
+        system = "x86_64-linux";
+        homeDirectory = "/home/dsn";
+        username = "dsn";
+        stateVersion = "22.05";
+        configuration = import ./home.nix;
       };
-
-      dsn = self.homeConfigurations.dsn.activationPackage;
-      defaultPackage.x86_64-linux = self.dsn;
-
-      # homeConfigurations.dsn = mkHomeConfiguration {
-      #     system = "x86_64-linux";
-      #     homeDirectory = "/home/dsn";
-      #     username = "dsn";
-      #     configuration = { config, pkgs, ...}: {
-      #       imports = [ ./home.nix ];
-      #     };
-      #     stateVersion = "22.05";
-      # };
-
-      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-      # defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
     };
+
+  };
 }
---
+++ nvim/lua/dsn/formatter.lua
@@ -0,0 +1,30 @@
+require('formatter').setup({
+  filetype = {
+    javascript = {
+      -- prettier
+      function()
+        return {
+          exe = "prettier",
+          args = {
+            "--config-precedence",
+            "prefer-file",
+            "--stdin-filepath",
+            vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+          },
+          stdin = true
+        }
+      end
+    },
+    nix = {
+			function()
+				return {
+					exe = "nixfmt",
+					args = {
+						vim.fn.shellescape(vim.api.nvim_buf_get_name(0)),
+					},
+          stdin = false
+				}
+			end
+    }
+  }
+})

d59ef932d65981332f1fd07da24fbd51afbb3d87
dsn <dsn@dsnn.io>
2022-01-31 22:37:12 +0100

    mount

--- hosts/test/configuration.nix
+++ hosts/test/configuration.nix
@@ -92,6 +92,7 @@
     vim 
     wget
     home-manager
+    cifs-utils
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -109,6 +110,20 @@
    enable = true;
    permitRootLogin = "yes";
   };
+
+  services.samba = {
+    enable = true;
+  };
+
+  fileSystems."/mnt/private" = {
+      device = "//dss/private";
+      fsType = "cifs";
+      options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+
+      in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
+  };  
 
   # ZFS services
   services.zfs.autoSnapshot.enable = true;

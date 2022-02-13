451671807214842389d62bdbf6b6320cc6f72c3b
dsn <dsn@dsnn.io>
2022-02-13 12:13:33 +0100

    pulseaudio

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -87,6 +87,8 @@
   # Enable sound.
   # sound.enable = true;
   hardware.pulseaudio.enable = true;
+  hardware.pulseaudio.support32Bit = true;
+  hardware.pulseaudio.package = pkgs.pulseaudioFull;
 
   environment.systemPackages = with pkgs; [
     cifs-utils

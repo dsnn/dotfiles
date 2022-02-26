f027c10871120f8050a42d21ac90bb146c86d419
dsn <dsn@dsnn.io>
2022-02-26 23:32:14 +0100

    fix up

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -11,7 +11,6 @@ in {
 
   # secrets specifications
   # for more options, e.g. permissions: https://github.com/Mic92/sops-nix#deploy-example
-  # sops.secrets.user-password.neededForUsers = true;
   sops.secrets.samba-credentials = { };
   sops.secrets.nmconnection-work-vpn = { };
 
@@ -135,7 +134,6 @@ in {
     extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" "plugdev" ];
     hashedPassword =
       "$6$n0/53jiplgIPWu8s$m4xx3iAHaYbQBxDtxLWFB0tnO0NpHl761ZgD3piAZkhQyMXRwcGGApDUKTF841PneckL9MgljztMRlx5MNyF70";
-    # passwordFile = config.sops.secrets.user-password.path;
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCblbdi9GiPOhBlH1aSn3+/0w8w7OVP+jNVbjX0iOf31WMJpyGi8X1ybsZfjrAQ2VoHuX/dN1BJlvOGO36PcDRsXDKE/+Db9VcJR8vzs4d1Nik8lbmjXgWHPv6Ig8SDVrqanV/6Yv9AbgZFqIbfqIsW41i/zkVt8wXYewATI6bjHs5gWox+5h/NBBu6bTCD1He4I8v6/1Dg3D/9o0fmhrwGOdd7W1zxPorjUC9uziUCc4uOnnTH5n1K59TvMYeUsdYtkToew7b1fJAsC1FY09GrgyQ+y+O07oGNLI9NyckEMIi+1hsSi3dNwLG2Y/lqcHM/YgdY3iez63h+W02tEuaF"
     ];

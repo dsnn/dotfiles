1fc07361e69403dd9967b1d4ae2028a0d9c618d8
dsn <dsn@dsnn.io>
2022-02-27 17:34:39 +0100

    modules & server

--- hosts/desktop.nix
+++ hosts/desktop.nix
@@ -19,24 +19,16 @@ in {
   ];
 
   nixpkgs.overlays = [ overlays ];
-
-  # allow proprietary packages
   nixpkgs.config.allowUnfree = true;
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  networking.hostId = "55aa39de";
+  networking.hostName = "dsn";
 
   networking.useDHCP = false;
   networking.interfaces.enp6s0.useDHCP = true;
   # networking.interfaces.wlp4s0.useDHCP = false;
   # networking.interfaces.enp5s0.useDHCP = true;
 
-  #  network id & hostname
-  networking.hostId = "55aa39de";
-  networking.hostName = "dsn";
-
-  # system packages
   environment.systemPackages = with pkgs; [
     cifs-utils
     coreutils
--- modules/xrdp.nix
+++ modules/xrdp.nix
@@ -1,7 +1,6 @@
-{ config, pkgs, ... }:
-{
-  # services.xrdp.enable = true;
-  # services.xrdp.port = 3389;
-  # services.xrdp.openFirewall = true;
+{ config, pkgs, ... }: {
+  services.xrdp.enable = true;
+  services.xrdp.port = 3389;
+  services.xrdp.openFirewall = true;
   # services.xrdp.defaultWindowManager = "${pkgs.awesome}/bin/awesome";
 }

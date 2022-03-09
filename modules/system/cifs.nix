abdd9acf2de76346acd0bd23a35848d15511b8ba
dsn <dsn@dsnn.io>
2022-03-09 22:31:55 +0100

    fix cifs

--- modules/system/cifs.nix
+++ modules/system/cifs.nix
@@ -3,14 +3,10 @@ let
   cifsShare = name: {
     device = "//dss/${name}";
     fsType = "cifs";
-    options = [
-      "x-systemd.automount"
-      "noauto"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5s"
-      "x-systemd.mount-timeout=5s"
-      "credentials=${config.sops.secrets.samba-credentials.path}"
-    ];
+    options = let
+      automount_opts =
+        "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in [ "${automount_opts},credentials=/etc/nixos/smb-secrets" ];
   };
 in {
 

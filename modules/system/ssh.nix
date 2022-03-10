37310e881cb57385c214f7ac5b6fada4dc4d1f81
dsn <dsn@dsnn.io>
2022-03-11 00:20:11 +0100

    ssh hardening

--- modules/system/ssh.nix
+++ modules/system/ssh.nix
@@ -4,11 +4,12 @@
   services.openssh.enable = true;
 
   # disable X11 connections to be forwarded
-  # services.openssh.forwardX11 = false;
+  services.openssh.forwardX11 = false;
 
   # disable root login 
-  # services.openssh.permitRootLogin = "no";
+  services.openssh.permitRootLogin = "no";
 
   # disable password auth
-  # services.openssh.passwordAuthentication = false;
+  services.openssh.passwordAuthentication = false;
+
 }

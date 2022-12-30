567c0a7b0481447498bd07b631fd69a3f8fc85fc
dsn <dsn@dsnn.io>
2022-12-30 14:09:45 +0100

    fix(nvim): luasnip console.log fmt test

--- files/nvim/snippets/typescriptreact.lua
+++ files/nvim/snippets/typescriptreact.lua
@@ -1,14 +1,18 @@
 local ls = require("luasnip")
+local fmt = require("luasnip.extras.fmt").fmt
 local i = ls.insert_node
 local s = ls.snippet
 local t = ls.text_node
 
 return {
-    s("ec", {
-      t("export const "),
-      i(1, "name"),
-      t(" = ("),
-      i(2, "data:any"),
-      t({") => {", "\treturn null;", "};"}),
-    }),
+  s("ec", {
+    t("export const "),
+    i(1, "name"),
+    t(" = ("),
+    i(2, "data:any"),
+    t({ ") => {", "\treturn null;", "};" }),
+  }),
+
+  -- console.log
+  s("log", fmt("console.log(\"result: \", {});", i(1))),
 }

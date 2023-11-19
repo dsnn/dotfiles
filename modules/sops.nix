b753e6164ee2f17188ab26aa50a576cf54e58c66
dsn <dsn@dsnn.io>
2023-11-19 13:14:07 +0100

    cleanup

--- modules/sops.nix
+++
@@ -1,6 +0,0 @@
-{
-
-  # for more options, e.g. permissions: https://github.com/Mic92/sops-nix#deploy-example
-  sops.defaultSopsFile = ../secrets/secrets.yaml;
-  sops.age.keyFile = "/home/dsn/.config/sops/age/keys.txt";
-}

c545d5eadd81a5e68eabcd5fdcea9806c13eb29a
dsn <dsn@dsnn.io>
2023-11-20 23:38:38 +0100

    desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -1,77 +1,127 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running `nixos-help`).
+
 { config, pkgs, ... }:
-let overlays = import ../../modules/system/overlays.nix;
-in {
-  imports = [
-    ./hardware.nix
-    ../../modules/system/boot.nix
-    ../../modules/system/zfs.nix
-    ../../modules/system/nix.nix
-    ../../modules/system/locale.nix
-    ../../modules/system/timezone.nix
-    ../../modules/system/ssh.nix
-    ../../modules/system/pulseaudio.nix
-    ../../modules/system/ergodox.nix
-    ../../modules/system/networkmanager.nix
-    ../../modules/system/cifs.nix
-    ../../modules/system/user.nix
-    ../../modules/sops.nix
-  ];
 
-  nixpkgs.overlays = [ overlays ];
-  nixpkgs.config.allowUnfree = true;
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  networking.hostId = "55aa39de";
-  networking.hostName = "dsn";
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.useDHCP = false;
-  networking.interfaces.enp6s0.useDHCP = true;
-  # networking.interfaces.wlp4s0.useDHCP = false;
-  # networking.interfaces.enp5s0.useDHCP = true;
+  networking.hostName = "alpha"; # Define your hostname.
+  networking.hostId = "199f97e0";
 
-  # persist settings for some apps (https://nixos.wiki/wiki/I3#DConf)
-  programs.dconf.enable = true;
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # enable the X11 windowing system
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.xserver.layout = "se, us";
-  services.xserver.dpi = 120;
-  services.xserver.xkbOptions = "eurosign:e,grp:alt_space_toggle";
 
-  # video drivers
-  services.xserver.videoDrivers = [ " nvidia " ];
 
-  # window manager
+  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.defaultSession = "none+i3";
-  services.xserver.windowManager.i3.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  
 
-  # work vpn profile
-  sops.secrets.nmconnection-work-vpn = { };
-  environment.etc."NetworkManager/system-connections/work.nmconnection".source =
-    "${config.sops.secrets.nmconnection-work-vpn.path}";
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e,caps:escape";
 
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.dsn = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    hashedPassword = "$y$j9T$Wg1CGw.yYxfHmeXp.joE3/$Z70N2uCHfh2BcQ978valtj/FByc3jwX.3q94hzD39U0";
+  };
+
+  nix = 
+   let 
+      users = [ "root" "dsn" ]; 
+   in {
+    settings = {
+      experimental-features = "nix-command flakes";
+      http-connections = 50; 
+      warn-dirty = false; 
+      log-lines = 50; 
+      sandbox = "relaxed"; 
+      auto-optimise-store = true; 
+      trusted-users = users; 
+      allowed-users = users; 
+    };
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
   environment.systemPackages = with pkgs; [
-    cifs-utils
-    coreutils
-    gcc
+    vim 
     git
-    home-manager
-    man
-    tree
-    vim
+    neovim
     wget
-    zsh
-    pciutils
-    pavucontrol
-    wally-cli
-    age
-    sops
-    docker
-    docker-compose
-    inetutils
   ];
 
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11";
+  system.stateVersion = "23.05"; # Did you read the comment?
+
 }
+

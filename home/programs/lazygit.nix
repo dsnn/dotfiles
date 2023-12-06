1acf0904092e42a2fb86d2b60101c381b8d7bb9f
dsn <dsn@dsnn.io>
2023-12-06 21:03:17 +0100

    lazygit

--- home/programs/lazygit.nix
+++ home/programs/lazygit.nix
@@ -3,7 +3,18 @@
   # TODO: https://github.com/catppuccin/lazygit
   # https://github.com/nix-community/home-manager/blob/master/modules/programs/lazygit.nix
 
-  programs.lazygit.enable = true;
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --color-only --dark --paging=never";
+          useConfig = false;
+        };
+      };
+    };
+  };
 
   programs.zsh.initExtra = ''
     function run_lazy_git() {

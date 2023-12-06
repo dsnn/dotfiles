a0a112035b20bf64fb782630d2b0cc423e0cb9c9
dsn <dsn@dsnn.io>
2023-12-06 21:03:48 +0100

    cleanup & packages

--- home/packages.nix
+++ home/packages.nix
@@ -16,6 +16,31 @@ with pkgs;
   wakeonlan
   xclip
   # sstp
+
+  # neovim
+  # nodePackages."bash-language-server"
+  # nodePackages."diagnostic-languageserver"
+  # nodePackages."dockerfile-language-server-nodejs"
+  # nodePackages."pyright"
+  # nodePackages."typescript"
+  # nodePackages."typescript-language-server"
+  # nodePackages."vscode-langservers-extracted"
+  # nodePackages."yaml-language-server"
+  # omnisharp-roslyn
+  # terraform
+  # terraform-ls
+  fd
+  lazydocker
+  lua-language-server
+  nil
+  nil
+  nixd
+  nixfmt
+  nixpkgs-fmt
+  ripgrep
+  rnix-lsp
+  shfmt
+# dotnet-sdk_8
 ] ++ pkgs.lib.optionals isServer [
   _1password
 ]

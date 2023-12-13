9e95bde89bc921ccbe2f6acd20330a2ea0a9b1da
dsn <dsn@dsnn.io>
2023-12-13 22:35:10 +0100

    sops (wip)

--- system/sops.nix
+++ system/sops.nix
@@ -1,10 +1,12 @@
-{ config, inputs, lib, ... }:
+{ config, pkgs, inputs, lib, ... }:
 let
+  inherit (pkgs.stdenv) isDarwin;
+  home = if isDarwin then "/Users/dsn" else "/home/dsn";
   defaultSopsPath =
     "${toString inputs.self}/hosts/${config.networking.hostName}/secrets.yaml";
 in {
 
-  sops.age.keyFile = "/home/dsn/.config/sops/age/keys.txt";
+  sops.age.keyFile = "${home}/.config/sops/age/keys.txt";
 
   sops.defaultSopsFile =
     lib.mkIf (builtins.pathExists defaultSopsPath) defaultSopsPath;

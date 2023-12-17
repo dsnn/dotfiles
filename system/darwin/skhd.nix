c747abd5a0daf9961d55ee617b4d0df1c71b4a67
dsn <dsn@dsnn.io>
2023-12-17 02:04:49 +0100

    skhd keymaps

--- system/darwin/skhd.nix
+++ system/darwin/skhd.nix
@@ -8,39 +8,39 @@ in {
     services.skhd = {
       enable = true;
       skhdConfig = ''
-        # change window focus within space
+        # change focus
+        alt - h : yabai -m window --focus west
         alt - j : yabai -m window --focus south
         alt - k : yabai -m window --focus north
-        alt - h : yabai -m window --focus west
         alt - l : yabai -m window --focus east
 
-        # swap windows
+        # swap
+        shift + alt - h : yabai -m window --swap west
         shift + alt - j : yabai -m window --swap south
         shift + alt - k : yabai -m window --swap north
-        shift + alt - h : yabai -m window --swap west
         shift + alt - l : yabai -m window --swap east
 
-        # move window and split
+        # move
+        ctrl + alt - h : yabai -m window --warp west
         ctrl + alt - j : yabai -m window --warp south
         ctrl + alt - k : yabai -m window --warp north
-        ctrl + alt - h : yabai -m window --warp west
         ctrl + alt - l : yabai -m window --warp east
 
-        # change focus between external displays (left and right)
+        # change focus (displays)
         alt - s: yabai -m display --focus west
         alt - g: yabai -m display --focus east
 
-        # rotate layout
-        shift + alt - r : yabai -m space --rotate 270
+        # rotate
+        alt - r : yabai -m space --rotate 90
 
-        # toggle window float
-        shift + alt - t : yabai -m window --toggle float --grid 4:4:1:1:2:2
+        # fullscreen
+        alt - f : yabai -m window --toggle zoom-fullscreen
 
-        # maximize a window
-        shift + alt - m : yabai -m window --toggle zoom-fullscreen
+        # toggle float
+        alt - t : yabai -m window --toggle float --grid 4:4:1:1:2:2
 
-        # balance out tree of windows (resize to occupy same area)
-        shift + alt - e : yabai -m space --balance
+        # balance
+        alt - e : yabai -m space --balance
 
         # move window to space #
         shift + alt - 1 : yabai -m window --space 1;

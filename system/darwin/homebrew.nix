d5a46dc2caf34dfabfb10ceb23861f352a69a9ab
dsn <dsn@dsnn.io>
2023-12-29 20:58:51 +0100

    enable on import brew, shkhd and yabai

--- system/darwin/homebrew.nix
+++ system/darwin/homebrew.nix
@@ -1,35 +1,27 @@
-{ lib, config, ... }:
-with lib;
-let cfg = config.mine.services.homebrew;
-in {
-  options.mine.services.homebrew = {
-    enable = mkEnableOption "Enable homebrew";
-  };
+{ ... }: {
 
-  config = mkIf cfg.enable {
-    homebrew = {
+  homebrew = {
 
-      enable = true;
-      caskArgs.no_quarantine = true;
-      global.brewfile = true;
-      taps = [
-        "fujiapple852/trippy"
-        "homebrew/core"
-        "homebrew/cask"
-        "homebrew/cask-fonts"
-      ];
-      brews = [ "mas" "sstp-client" "trippy" "git-crypt" ];
-      casks = [
-        "arc"
-        # "raycast"
-        # "firefox"
-        # "spotify"
-        # "slack"
-        # "visual-studio-code"
-        # "zoom"
-        # "obs"
-        # "font-sf-mono-nerd-font"
-      ];
-    };
+    enable = true;
+    caskArgs.no_quarantine = true;
+    global.brewfile = true;
+    taps = [
+      "fujiapple852/trippy"
+      # "homebrew/core"
+      # "homebrew/cask"
+      # "homebrew/cask-fonts"
+    ];
+    brews = [ "mas" "sstp-client" "trippy" "git-crypt" ];
+    casks = [
+      # "arc"
+      # "raycast"
+      # "firefox"
+      # "spotify"
+      # "slack"
+      # "visual-studio-code"
+      # "zoom"
+      # "obs"
+      # "font-sf-mono-nerd-font"
+    ];
   };
 }

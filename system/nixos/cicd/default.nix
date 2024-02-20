7c945f78c0fc9ae107b234a0c519f0e8ee2ea486
dsn <dsn@dsnn.io>
2024-02-20 16:35:58 +0100

    disable drone runner exec

--- system/nixos/cicd/default.nix
+++ system/nixos/cicd/default.nix
@@ -3,7 +3,7 @@
   imports = [
     ./docker-registry.nix
     ./drone-runner-docker.nix
-    ./drone-runner-exec.nix
+    # ./drone-runner-exec.nix # binds to port 3000 by defeault. Change port for gitea (at port 3000 atm) ?
     ./drone-srv.nix
     ./gitea.nix
     ../services/nginx.nix

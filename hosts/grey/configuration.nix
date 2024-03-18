3bb8b7057c6b7fdf2d2fa5b10c32b4c41210aca6
dsn <dsn@dsnn.io>
2024-03-18 12:47:05 +0100

    cups

--- hosts/grey/configuration.nix
+++ hosts/grey/configuration.nix
@@ -15,6 +15,45 @@
     ./hardware.nix
   ];
 
+  services.printing = {
+    enable = true;
+    listenAddresses = [ "*:631" ];
+    allowFrom = [ "all" ];
+    browsing = true;
+    defaultShared = true;
+    openFirewall = true;
+    drivers = with pkgs; [ cups-bjnp gutenprint ];
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+    openFirewall = true;
+    publish = {
+      enable = true;
+      userServices = true;
+    };
+  };
+
+  # networking.wg-quick.interfaces = {
+  #    wg0 = {
+  #      address = [ "192.168.1.1/24" "fdc9:281f:04d7:9ee9::2/64" ];
+  #      dns = [ "10.0.0.1" "fdc9:281f:04d7:9ee9::1" ];
+  #      privateKeyFile = "/root/wireguard-keys/privatekey";
+  #
+  #      peers = [
+  #        {
+  #          publicKey = "{server public key}";
+  #          presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+  #          allowedIPs = [ "0.0.0.0/0" "::/0" ];
+  #          endpoint = "{server ip}:51820";
+  #          persistentKeepalive = 25;
+  #        }
+  #      ];
+  #    };
+  #  };
+  #
+
   nixpkgs.config.allowUnfree = true;
 
   boot.loader.systemd-boot.enable = true;

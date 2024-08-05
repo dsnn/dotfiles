2f3930ad95e3f17d055a97580e2f82eb2a416ab9
dsn <dsn@dsnn.io>
2024-08-05 13:41:39 +0200

    autoUpgrade WIP

--- system/nixos/system.nix
+++ system/nixos/system.nix
@@ -1,6 +1,10 @@
 { ... }: {
   system.autoUpgrade = {
     enable = true;
+    # flake = inputs.self.outPath;
     channel = "https://nixos.org/channels/nixos-24.05";
+    # flags = [ "--update-input" "nixpkgs" "-L" ];
+    # dates = "09:00";
+    # randomizeDelaySec = "45min";
   };
 }

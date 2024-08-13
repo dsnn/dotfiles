cbe7354dffb238ec21939f5e5019cd20ef729c39
dsn <dsn@dsnn.io>
2024-08-13 19:47:04 +0200

    colmena (WIP)

---
+++ hive.nix
@@ -0,0 +1,10 @@
+{
+  meta = { nixpkgs = <nixpkgs>; };
+  # colmena = {
+  #   template = import ./configs/template.nix {
+  #     targetHost = "192.168.2.134";
+  #     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+  #     inherit inputs;
+  #   };
+  # };
+}

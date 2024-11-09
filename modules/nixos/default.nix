cf4a70fe52e9af9f4975145197bfaa20b13005a4
dsn <dsn@dsnn.io>
2024-11-09 12:41:05 +0100

    rename modules

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,28 +3,28 @@
   # imports = scanPaths ./.;
   imports = [
     ./auto-upgrade.nix
-    ./avahi.nix
     ./awesomewm
     ./cifs.nix
-    ./cups.nix
-    ./docker-registry.nix
-    ./fail2ban.nix
-    ./gitea.nix
-    ./homepage-dashboard.nix
     ./i18n.nix
-    ./jellyfin.nix
-    ./k3s-agent.nix
-    ./k3s-srv.nix
-    ./nginx.nix
     ./nix.nix
     ./packages.nix
-    ./postgres.nix
-    ./prometheus.nix
     ./security.nix
+    ./service-avahi.nix
+    ./service-cups.nix
+    ./service-docker-registry.nix
+    ./service-fail2ban.nix
+    ./service-gitea.nix
+    ./service-homepage-dashboard.nix
+    ./service-jellyfin.nix
+    ./service-k3s-agent.nix
+    ./service-k3s-srv.nix
+    ./service-nginx.nix
+    ./service-openssh.nix
+    ./service-postgres.nix
+    ./service-prometheus.nix
+    ./service-traefik.nix
     ./shells.nix
     ./sops.nix
-    ./openssh.nix
-    ./traefik.nix
     ./users.nix
     ./virtualisation.nix
     ./wireguard.nix

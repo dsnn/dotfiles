5c02c87227cedea7cd6601c9b88da2f9607fdee1
dsn <dsn@dsnn.io>
2025-08-01 15:15:58 +0200

    silver zsh

--- modules/silver/zsh.nix
+++ modules/silver/zsh.nix
@@ -11,10 +11,10 @@
       '';
 
       shellAliases = {
-        cfc = "vim $HOME/dotfiles/modules/silver/home.nix";
-        cfg = "vim $HOME/dotfiles/modules/home/git.nix";
-        cfh = "vim $HOME/dotfiles/profiles/dsn.nix";
-        cfz = "vim $HOME/dotfiles/modules/home/zsh.nix";
+        cfc = "vim $HOME/dotfiles/modules/silver/configurations.nix";
+        cfh = "vim $HOME/dotfiles/modules/silver/home.nix";
+        cfg = "vim $HOME/dotfiles/modules/git.nix";
+        cfz = "vim $HOME/dotfiles/modules/silver/zsh.nix";
         rf = "home-manager switch --flake ~/dotfiles/#silver; source ~/.config/zsh/.zshrc";
         rs = "sudo darwin-rebuild switch --flake ~/dotfiles/#silver";
         ru = "pushd ~/dotfiles; nix flake update; rf; popd";

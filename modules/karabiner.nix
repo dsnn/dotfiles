cdda5a542164daf6c74710e73f679e2a5258de24
dsn <dsn@dsnn.io>
2025-08-02 09:36:32 +0200

    karabiner

--- modules/karabiner.nix
+++ modules/karabiner.nix
@@ -2,8 +2,8 @@
   flake.modules.homeManager.karabiner =
     { config, ... }:
     let
+      source = "${config.home.homeDirectory}/dotfiles/modules/home/karabiner/src";
       destination = "${config.home.homeDirectory}.config/karabiner";
-      source = "${config.home.homeDirectory}/dotfiles/modules/home/_karabiner/src";
     in
     {
       home.file.${destination}.source = config.lib.file.mkOutOfStoreSymlink "${source}";

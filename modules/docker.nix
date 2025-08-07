d09cc99dbc9c25e96debbaf96c4064182b39fd05
dsn <dsn@dsnn.io>
2025-08-07 16:04:13 +0200

    docker

--- modules/docker.nix
+++ modules/docker.nix
@@ -1,38 +1,39 @@
 {
-
-  flake.modules.nixos.docker = {
-    programs.zsh.shellAliases = {
-      ds = "docker ps -a";
-      di = "docker images";
-      drm = ''docker rm $(docker ps -qa --no-trunc --filter "status=exited")'';
-      drmi = "docker rmi $(docker images -q -f dangling=true)";
-      # dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
-    };
-
-    virtualisation.docker = {
-      enable = true;
-      enableOnBoot = true;
-      storageDriver = "zfs";
-      autoPrune = {
-        enable = true;
-        dates = "weekly";
+  flake.modules.nixos.docker =
+    { config, lib, ... }:
+    {
+      programs.zsh.shellAliases = lib.mkIf config.programs.zsh.enable {
+        ds = "docker ps -a";
+        di = "docker images";
+        drm = ''docker rm $(docker ps -qa --no-trunc --filter "status=exited")'';
+        drmi = "docker rmi $(docker images -q -f dangling=true)";
+        # dalias() { alias | grep 'docker' | sed "s/^\([^=]*\)=\(.*\)/\1 => \2/"| sed "s/['|\']//g" | sort; }
       };
-    };
 
-    # TODO: review this
-    # systemd = {
-    #   timers.docker-prune = {
-    #     wantedBy = [ "timers.target" ];
-    #     partOf = [ "docker-prune.service" ];
-    #     timerConfig.OnCalendar = "*-*-* 2:00:00";
-    #   };
-    #   services.docker-prune = {
-    #     serviceConfig.Type = "oneshot";
-    #     script = ''
-    #       ${pkgs.docker}/bin/docker image prune --filter "until=72h"
-    #     '';
-    #     requires = [ "docker.service" ];
-    #   };
-    # };
-  };
+      virtualisation.docker = {
+        enable = true;
+        enableOnBoot = true;
+        storageDriver = "zfs";
+        autoPrune = {
+          enable = true;
+          dates = "weekly";
+        };
+      };
+
+      # TODO: review this
+      # systemd = {
+      #   timers.docker-prune = {
+      #     wantedBy = [ "timers.target" ];
+      #     partOf = [ "docker-prune.service" ];
+      #     timerConfig.OnCalendar = "*-*-* 2:00:00";
+      #   };
+      #   services.docker-prune = {
+      #     serviceConfig.Type = "oneshot";
+      #     script = ''
+      #       ${pkgs.docker}/bin/docker image prune --filter "until=72h"
+      #     '';
+      #     requires = [ "docker.service" ];
+      #   };
+      # };
+    };
 }

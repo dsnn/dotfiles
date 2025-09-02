96253fe2724ea8e77eca6a2dfe727ad1d303b11b
dsn <dsn@dsnn.io>
2025-09-02 23:14:45 +0200

    fix fonts and icons

--- modules/hosts/silver/home.nix
+++ modules/hosts/silver/home.nix
@@ -21,13 +21,7 @@
       ];
 
       font-pkacges = with pkgs; [
-        noto-fonts
-        noto-fonts-cjk-sans
-        noto-fonts-emoji
-        liberation_ttf
-        fira-code
-        fira-code-symbols
-        # Iosevka-nerd-font
+        nerd-fonts.meslo-lg
       ];
 
       dev-packages = with pkgs; [

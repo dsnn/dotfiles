bc7195843b21dd77d9f87721570a87a61f8d828b
dsn <dsn@dsnn.io>
2026-03-17 19:15:06 +0100

    nixos filesystem

--- modules/hosts/nixos/settings.nix
+++ modules/hosts/nixos/settings.nix
@@ -3,8 +3,37 @@
   flake.modules.homeManager.nixos.home.homeDirectory = "/home/dsn";
 
   flake.modules.nixos.nixos =
-    { pkgs, ... }:
     {
+      lib,
+      pkgs,
+      nixpkgs,
+      modulesPath,
+      ...
+    }:
+    {
+      imports = [
+        (modulesPath + "/profiles/qemu-guest.nix")
+      ];
+
+      boot.initrd.availableKernelModules = [
+        "ata_piix"
+        "uhci_hcd"
+        "virtio_pci"
+        "virtio_scsi"
+        "sd_mod"
+        "sr_mod"
+      ];
+      boot.initrd.kernelModules = [ ];
+      boot.kernelModules = [ ];
+      boot.extraModulePackages = [ ];
+
+      fileSystems."/" = {
+        device = "/dev/disk/by-uuid/cb735fc2-2eff-456b-abd4-da7d0368643d";
+        fsType = "ext4";
+      };
+
+      swapDevices = [ ];
+
       networking = {
         hostName = "nixos";
         dhcpcd.enable = true;

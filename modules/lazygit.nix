52a875ded9a91837acee8f142d10c65568a8352a
dsn <dsn@dsnn.io>
2026-04-01 00:27:35 +0200

    lazygit

--- modules/lazygit.nix
+++ modules/lazygit.nix
@@ -8,6 +8,23 @@
       programs.lazygit = {
         enable = true;
         settings = {
+          # Catppuccin mocha
+          gui.theme = {
+            lightTheme = false; # mocha is dark
+
+            activeBorderColor = [
+              "#89b4fa"
+              "bold"
+            ];
+            inactiveBorderColor = [ "#a6adc8" ];
+            optionsTextColor = [ "#89b4fa" ];
+            selectedLineBgColor = [ "#313244" ];
+            cherryPickedCommitBgColor = [ "#45475a" ];
+            cherryPickedCommitFgColor = [ "#89b4fa" ];
+            unstagedChangesColor = [ "#f38ba8" ];
+            defaultFgColor = [ "#cdd6f4" ];
+            searchingActiveBorderColor = [ "#f9e2af" ];
+          };
           git = {
             paging = {
               colorArg = "always";
